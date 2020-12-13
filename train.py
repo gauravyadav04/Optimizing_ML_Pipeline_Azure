5019e46d08250d41fb868067ecaa890ec2f2cd6a
Gaurav <gaurav.yadav04@gmail.com>
2020-12-13 18:27:18 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
+	
+	# Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
     jobs = pd.get_dummies(x_df.job, prefix="job")
     x_df.drop("job", inplace=True, axis=1)
@@ -34,8 +34,8 @@ def clean_data(data):
     x_df["month"] = x_df.month.map(months)
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+	
+	y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
 	
 	return x_df, y_df
 
@@ -44,13 +44,13 @@ def clean_data(data):
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
 ### YOUR CODE HERE ###
-ds =  TabularDatasetFactory.from_delimited_files(["https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"])
+ds =  TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+### YOUR CODE HERE ###
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -61,18 +61,18 @@ run = Run.get_context()
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+	
+	parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
-    args = parser.parse_args()
-
-    run.log("Regularization Strength:", np.float(args.C))
+	
+	args = parser.parse_args()
+	
+	run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
-
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-
-    accuracy = model.score(x_test, y_test)
+	
+	model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+	
+	accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
